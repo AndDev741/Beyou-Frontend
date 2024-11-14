7c69af32788843ae34954110eaffe11ddab7108d
AndDev741 <anddesenvolved@gmail.com>
2024-11-14 14:40:08 -0300

    feat: Now chooseInput have numeric values

--- src/components/inputs/chooseInput.jsx
+++ src/components/inputs/chooseInput.jsx
@@ -7,14 +7,14 @@ function ChooseInput({choosedLevel, error, name, setLevel, levels, title, t}){
                 className={labelCss}>{t(`${title}`)}</label>
                 <p className='text-red-500 text-lg underline'>{error}</p>
                 <div className="flex flex-row items-center justify-evenly w-full mt-2">
-                    {levels.map((level) => (
+                    {levels.map((level, index) => (
                         <div key={level}
                         className="flex flex-col items-center cursor-pointer">
                             <input type="radio"
                             onChange={(e) => setLevel(e.target.value)}
                             name={name}
                             id={level}
-                            value={level}
+                            value={index + 1}
                             className="border-0 w-full h-[20px] outline-none" />
                             <label htmlFor={level}
                             className={`${choosedLevel === level ? "text-blueMain" : ""}`}>{level}</label>
