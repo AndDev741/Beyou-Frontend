d9e4c3849871187348a43fc4961f38fdf6d69274
AndDev741 <anddesenvolved@gmail.com>
2025-02-06 15:30:27 -0300

    fix: Verification of length in icons render

--- src/components/icons/iconsRender.js
+++ src/components/icons/iconsRender.js
@@ -7,20 +7,20 @@ function iconRender(search, selectedIcon, icons) {
   }));
 
   let randomIcons =  icons || [];
-  if(search.length < 2 && selectedIcon === null){
+  if(search?.length < 2 && selectedIcon === null){
     randomIcons = iconArray
     .sort(() => 0.5 - Math.random())
     .slice(0, 28);
   }
 
   let filteredIcons = [];
-  if (search.length >= 2) {
+  if (search?.length >= 2) {
     filteredIcons = iconArray.filter((icon) => 
       icon.name.toLowerCase().includes(search.toLowerCase())
     );
   }
 
-  const iconsToDisplay = search.length >= 2 ? filteredIcons : randomIcons;
+  const iconsToDisplay = search?.length >= 2 ? filteredIcons : randomIcons;
 
   return iconsToDisplay;
 }
