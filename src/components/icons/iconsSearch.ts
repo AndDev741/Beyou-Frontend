062829db66aab72f4aa50b748c19c5c95e106d45
AndDev741 <anddesenvolved@gmail.com>
2025-02-25 10:05:02 +0000

    feat: iconSearch are now typed with typescript

---
+++ src/components/icons/iconsSearch.ts
@@ -0,0 +1,18 @@
+import * as MdIcons from "react-icons/md";
+import { IconName } from "../../types/icons/IconName";
+import { IconType } from "react-icons";
+import { IconObject } from "../../types/icons/IconComponent";
+
+function iconSearch(name: string){
+    const iconArray: IconObject[] = Object.keys(MdIcons).map((iconName) => ({
+        name: iconName as IconName,
+        iconComponent: MdIcons[iconName as IconName] as IconType,
+    }));
+
+    const icon = iconArray.filter((icon) => 
+        icon.name.toLowerCase().includes(name.toLowerCase()));
+
+    return icon[0];
+}
+
+export default iconSearch;
