a24654427dc4bc8f286da6b0310fcddd2b17bd75
AndDev741 <anddesenvolved@gmail.com>
2025-02-27 16:47:55 +0000

    feat: Usertyped created

---
+++ src/types/user/UserType.ts
@@ -0,0 +1,10 @@
+export type UserType = {
+    id: string,
+    name: string,
+    email: string,
+    phrase: string,
+    phrase_author: string,
+    constance: number,
+    photo: string,
+    isGoogleAccount: boolean
+}
