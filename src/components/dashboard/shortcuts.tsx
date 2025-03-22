ac6e5ba614b11c841e40264a9bca66292c029086
AndDev741 <anddesenvolved@gmail.com>
2025-03-22 14:14:39 +0000

    feat: Shortcuts to all funcionalities of the app

--- src/components/dashboard/shortcuts.tsx
+++ src/components/dashboard/shortcuts.tsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
 import categoriesIcon from "../../assets/dashboard/shortcuts/categories.svg";
-import habitsIcon from "../../assets/dashboard/shortcuts/habits.svg"
+import habitsIcon from "../../assets/dashboard/shortcuts/habits.svg";
+import tasksIcon from "../../assets/dashboard/shortcuts/taskIcon.svg";
+import routineIcon from "../../assets/dashboard/shortcuts/routineIcon.svg";
+import goalsIcon from "../../assets/dashboard/shortcuts/goalsIcon.svg";
+import configIcon from "../../assets/dashboard/shortcuts/configIcon.svg";
 import { useTranslation } from "react-i18next";
-import TranslationButton from "../translationButton";
 
 function Shortcuts(){
     const {t} = useTranslation();
@@ -12,7 +15,7 @@ function Shortcuts(){
         <div className="mt-6">
             <h2 className="text-center text-3xl font-semibold lg:hidden">{t('Shortcuts')}</h2>
 
-            <nav className="flex flex-wrap lg:flex-col justify-evenly items-start m-4">
+            <nav className="flex flex-wrap lg:flex-col justify-between items-start m-4">
                 <Link to={"/categories"}
                 className={linkStyle}>
                     <img className="w-[30px] mr-2" 
@@ -20,13 +23,40 @@ function Shortcuts(){
                     alt={t('CategoriesImgAlt')}/>                   
                     {t('Categories')}
                 </Link>
-                <TranslationButton />
                 <Link to={"/habits"}
                 className={linkStyle}>
                     <img className="w-[30px] mr-2" 
                     src={habitsIcon}
                     alt={t('HabitImgAlt')}/>                   
                     {t('Habits')}
+                </Link>
+                <Link to={"/tasks"}
+                className={linkStyle}>
+                    <img className="w-[30px] mr-2" 
+                    src={tasksIcon}
+                    alt={t('Tasks icon alt')}/>                   
+                    {t('Tasks')}
+                </Link>
+                <Link to={"/routines"}
+                className={linkStyle}>
+                    <img className="w-[30px] mr-2" 
+                    src={routineIcon}
+                    alt={t('Routines icon alt')}/>                   
+                    {t('Routines')}
+                </Link>
+                <Link to={"/goals"}
+                className={linkStyle}>
+                    <img className="w-[30px] mr-2" 
+                    src={goalsIcon}
+                    alt={t('Routines icon alt')}/>                   
+                    {t('Goals')}
+                </Link>
+                <Link to={"/configuration"}
+                className={linkStyle}>
+                    <img className="w-[30px] mr-2" 
+                    src={configIcon}
+                    alt={t('Routines icon alt')}/>                   
+                    {t('Config')}
                 </Link>
             </nav>
         </div>
