f66d2ef7de649bbbd0810dd8768c2e054cc4b6cc
anddev741 <anddesenvolved@gmail.com>
2025-05-25 14:20:43 +0100

    feat: Added new redux to rootReducer

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -5,13 +5,17 @@ import editCategorySlice from './category/editCategorySlice';
 import editHabitSlice from './habit/editHabitSlice';
 import errorHandlerSlice from './errorHandler/errorHandlerSlice';
 import editTaskSlice from "./task/editTaskSlice";
+import habitsSlice from "./habit/habitsSlice";
+import tasksSliced from "./task/tasksSlice";
 
 const rootReducer = combineReducers({
     register: registerSlice,
     perfil: perfilSlice,
     editCategory: editCategorySlice,
     editHabit: editHabitSlice,
+    habits: habitsSlice,
     editTask: editTaskSlice,
+    tasks: tasksSliced,
     errorHandler: errorHandlerSlice
 });
 
