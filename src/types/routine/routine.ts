2bf42d7906a14b14da175e738c079de24b4a675c
AndDev741 <anddesenvolved@gmail.com>
2025-05-27 14:27:05 +0100

    feat: change in routine type to afford the type of routine

--- src/types/routine/routine.ts
+++ src/types/routine/routine.ts
@@ -3,6 +3,7 @@ import { RoutineSection } from "./routineSection";
 export type Routine = {
     id?: string;
     name: string;
+    type?: string;
     iconId: string;
     routineSections: Array<RoutineSection>;
 }
