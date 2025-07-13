54818fd35aed7c76887a6ea4b60fc87f3b0bf359
AndDev741 <anddesenvolved@gmail.com>
2025-07-13 10:05:47 +0100

    refactor: Last refactorae in description input. WOrking fine now

--- src/components/inputs/descriptionInput.tsx
+++ src/components/inputs/descriptionInput.tsx
@@ -1,4 +1,5 @@
 import { TFunction } from "i18next";
+import { useEffect, useState } from "react";
 
 type descriptionInputProps = {
     description: string,
@@ -10,6 +11,16 @@ type descriptionInputProps = {
     t: TFunction
 }
 function DescriptionInput({description, setDescription, placeholder, descriptionError, minH, minHSmallScreen, t}: descriptionInputProps){
+    const [matches, setMatches] = useState(
+        window.matchMedia("(min-width: 768px)").matches
+    )
+
+    useEffect(() => {
+        window
+        .matchMedia("(min-width: 768px)")
+        .addEventListener('change', e => setMatches( e.matches ));
+    }, []);
+
     const borderCss = `border-solid border-[1px] border-blueMain rounded-md w-[45vw] ${minHSmallScreen ? `min-h-[${minHSmallScreen}px] h-[${minHSmallScreen}px]` : ""} md:w-[320px] lg:w-[15rem]`;
     const labelCss = "text-2xl md:text-xl";
     return(
@@ -20,12 +31,22 @@ function DescriptionInput({description, setDescription, placeholder, description
                     {t('Description')}
                 </label>
                 <p className='text-red-500 text-lg'>{descriptionError}</p>
-                <textarea id='description'
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-                name='description'
-                placeholder={t(`${placeholder}`)}
-                className={`${borderCss} ${descriptionError ? "border-red-500" : ""} outline-none text-lg p-1 ] ${minH ? `lg:min-h-[${minH}px] lg:h-[${minH}px]` : "md:h-[161px] lg:min-h-[178px] "}`}
+                <textarea
+                    id="description"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    name="description"
+                    placeholder={t(`${placeholder}`)}
+                    className={`${borderCss} ${descriptionError ? "border-red-500" : ""} outline-none text-lg p-1`}
+                    style={{
+                        minHeight: minH ? `${minH}px` : undefined,
+                        height: minH ? `${minH}px` : undefined,
+                        ...(minHSmallScreen && !matches && {
+                        minHeight: `${minHSmallScreen}px`,
+                        height: `${minHSmallScreen}px`,
+                        
+                        })
+                    }}
                 />
             </div>
         </>
