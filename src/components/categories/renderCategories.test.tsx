33631146bb3c41e0b1e71a8d442dae08e5591e93
AndDev741 <anddesenvolved@gmail.com>
2026-01-02 10:15:19 +0000

    test: Addin i18next mock and router mock

--- src/components/categories/renderCategories.test.tsx
+++ src/components/categories/renderCategories.test.tsx
@@ -1,4 +1,5 @@
 import { screen } from "@testing-library/react";
+import { act } from "react";
 import Categories from "../../pages/categories/categories";
 import getCategories from "../../services/categories/getCategories";
 import { renderWithProviders } from "../../test/test-utils";
@@ -8,7 +9,9 @@ jest.mock('../../services/categories/getCategories.ts');
 test('Should show a custom message of 0 categories created', async () => {
     (getCategories as jest.Mock).mockResolvedValue({ success: [] });
 
-    renderWithProviders(<Categories />);
+    await act(async () => {
+        renderWithProviders(<Categories />);
+    });
 
     const message = await screen.findByText('0CategoriesMessage');
     expect(message).toBeInTheDocument();
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,8 +3,32 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import React from 'react';
 // Use manual mock to avoid parsing axios ESM bundle in Jest
 jest.mock('axios');
+
+// Lightweight i18n stub so components can call t() without a full instance
+jest.mock('react-i18next', () => ({
+  useTranslation: () => ({
+    t: (key: string) => key,
+    i18n: {
+      changeLanguage: () => Promise.resolve(),
+      language: 'en',
+    },
+  }),
+  Trans: ({ children }: { children: React.ReactNode }) => children,
+  initReactI18next: {
+    type: '3rdParty',
+    init: () => {},
+  },
+}));
+
+// Route act calls to React's version to silence react-dom/test-utils deprecation warning
+jest.mock('react-dom/test-utils', () => {
+  const actual = jest.requireActual('react-dom/test-utils');
+  const react = jest.requireActual('react');
+  return { ...actual, act: react.act };
+});
 
 // Polyfill matchMedia for components that rely on it
 if (!window.matchMedia) {
